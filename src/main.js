e429c1a048d11efcfb31453b8d9d736e170e5f5d
Valentina Ramirez <vramirezm@centro.edu.mx>
2026-05-24 23:01:10 -0600

    StartScreen + CSS + JS

--- src/main.js
+++ src/main.js
@@ -1,60 +1,84 @@
 import './style.css'
-import javascriptLogo from './assets/javascript.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import { setupCounter } from './counter.js'
+import * as THREE from 'three'
+import gsap from 'gsap'
 
-document.querySelector('#app').innerHTML = `
-<section id="center">
-  <div class="hero">
-    <img src="${heroImg}" class="base" width="170" height="179">
-    <img src="${javascriptLogo}" class="framework" alt="JavaScript logo"/>
-    <img src="${viteLogo}" class="vite" alt="Vite logo" />
-  </div>
-  <div>
-    <h1>Get started</h1>
-    <p>Edit <code>src/main.js</code> and save to test <code>HMR</code></p>
-  </div>
-  <button id="counter" type="button" class="counter"></button>
-</section>
+console.log('Hello Three.js!')
+console.log('Hello GSAP!')
 
-<div class="ticks"></div>
 
-<section id="next-steps">
-  <div id="docs">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#documentation-icon"></use></svg>
-    <h2>Documentation</h2>
-    <p>Your questions, answered</p>
-    <ul>
-      <li>
-        <a href="https://vite.dev/" target="_blank">
-          <img class="logo" src="${viteLogo}" alt="" />
-          Explore Vite
-        </a>
-      </li>
-      <li>
-        <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-          <img class="button-icon" src="${javascriptLogo}" alt="">
-          Learn more
-        </a>
-      </li>
-    </ul>
-  </div>
-  <div id="social">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#social-icon"></use></svg>
-    <h2>Connect with us</h2>
-    <p>Join the Vite community</p>
-    <ul>
-      <li><a href="https://github.com/vitejs/vite" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#github-icon"></use></svg>GitHub</a></li>
-      <li><a href="https://chat.vite.dev/" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#discord-icon"></use></svg>Discord</a></li>
-      <li><a href="https://x.com/vite_js" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#x-icon"></use></svg>X.com</a></li>
-      <li><a href="https://bsky.app/profile/vite.dev" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#bluesky-icon"></use></svg>Bluesky</a></li>
-    </ul>
-  </div>
-</section>
 
-<div class="ticks"></div>
-<section id="spacer"></section>
-`
+const fonts = [ "Rubik Storm", "Coral Pixels", 
+    "Bitcount Grid Single", "Jacquard 12", "Bytesized", 
+    "Jacquarda Bastarda 9", "Micro 5 Charted" ];
 
-setupCounter(document.querySelector('#counter'))
+function rand(arr){ return arr[Math.floor(Math.random()*arr.length)]; }
+
+let lastFont = null;
+
+function getRandomFont(){
+  let newFont;
+  do {
+    newFont = rand(fonts);
+  } while (newFont === lastFont && fonts.length > 1);
+  lastFont = newFont;
+  return newFont;
+}
+
+function applyGlitch(){
+  const heads = document.querySelectorAll('#startScreen .glitch-target');
+  
+  // Quitar .active de todos
+  heads.forEach(h => h.classList.remove('active'));
+  
+  // Escoger uno aleatorio y activarlo
+  const randomHead = heads[Math.floor(Math.random() * heads.length)];
+  randomHead.classList.add('active');
+  randomHead.style.fontFamily = getRandomFont();
+  
+  // Activar glitch
+  randomHead.classList.add('glitch');
+  setTimeout(() => randomHead.classList.remove('glitch'), 200 + Math.random()*300);
+}
+
+// Ejecutar cada segundo
+applyGlitch();
+setInterval(applyGlitch, 1000);
+
+
+const container = document.getElementById('rain-container')
+
+const images = [
+  './src/assets/album_foto.png',
+  './src/assets/bts_dope.png',
+  './src/assets/comics.png',
+  './src/assets/ipad.png',
+  './src/assets/la_bruja.png',
+  './src/assets/maiz.png',
+  './src/assets/Monster_high.png',
+  './src/assets/rojo.png',
+]
+
+function createRainObject(){
+
+  const img = document.createElement('img')
+
+  img.src = images[Math.floor(Math.random() * images.length)]
+
+  img.classList.add('rain-object')
+
+  img.style.left = Math.random() * window.innerWidth + 'px'
+
+  img.style.animationDuration =
+    3 + Math.random() * 5 + 's'
+
+  img.style.width =
+    40 + Math.random() * 80 + 'px'
+
+  container.appendChild(img)
+
+  setTimeout(() => {
+    img.remove()
+  }, 8000)
+}
+
+setInterval(createRainObject, 200)
