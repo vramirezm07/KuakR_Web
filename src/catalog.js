d533dac8d0e7db7acb61202afac7d571564d1652
Valentina Ramirez <vramirezm@centro.edu.mx>
2026-06-08 21:13:27 -0600

    Objetos catálogo implementación - HTML, JS y CSS

--- src/catalog.js
+++ src/catalog.js
@@ -1,40 +1,46 @@
 import './style.css'
-import * as THREE from 'three'
-import gsap from 'gsap'
-import Swiper from 'swiper';
-import { Navigation } from 'swiper/modules';
+import './catalog.css'
+import Swiper from 'swiper'
+import { Navigation } from 'swiper/modules'
+import { CatalogVirus } from './catalogVirus.js'
+import { CatalogAnalogTV } from './catalogAnalogTV.js'
 
-
-import 'swiper/css';
-import 'swiper/css/navigation';
-
+import 'swiper/css'
+import 'swiper/css/navigation'
 
 const swiper = new Swiper('.swiper', {
   loop: true,
-  
-  
   touchReleaseOnEdges: true,
-  
-
   mousewheel: {
     releaseOnEdges: true,
   },
-
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-});
+})
 
-const botonSiguiente = document.querySelector('.swiper-button-next');
-const botonAnterior = document.querySelector('.swiper-button-prev');
+const botonSiguiente = document.querySelector('.swiper-button-next')
+const botonAnterior = document.querySelector('.swiper-button-prev')
 
 if (botonSiguiente && botonAnterior) {
   botonSiguiente.addEventListener('click', () => {
-    swiper.slideNext(); // Le ordena a Swiper avanzar
-  });
+    swiper.slideNext()
+  })
 
   botonAnterior.addEventListener('click', () => {
-    swiper.slidePrev(); // Le ordena a Swiper retroceder
-  });
+    swiper.slidePrev()
+  })
+}
+
+const virusOverlay = document.getElementById('virus-overlay')
+if (virusOverlay) {
+  const catalogVirus = new CatalogVirus(virusOverlay, swiper)
+  catalogVirus.bind()
+}
+
+const tvCanvas = document.getElementById('catalog-tv-canvas')
+if (tvCanvas) {
+  const catalogTV = new CatalogAnalogTV(tvCanvas)
+  catalogTV.start()
 }
